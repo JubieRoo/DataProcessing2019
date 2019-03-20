52be886805ccfc8448c5703769f1a7bac42eb607
Ruby Bron <rubybron@gmail.com>
2019-03-20 13:36:06 +0100

    created and linked files

---
+++ Homework/Week_6/final.js
@@ -0,0 +1,6 @@
+/*
+Name: Ruby Bron
+Studenid: 12474223
+Data source: https://www.kaggle.com/new-york-state/nys-biodiversity-by-county
+This file contains the script for the creation of the plots (chloropleth and bar-chart)
+*/
